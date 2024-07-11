2ebe060f07d90fb4d3eefa20bb1fc6174075c408
Reimanbow <reimanbow0318@gmail.com>
2024-07-11 18:16:07 +0900

    add: display

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "FS.h"
 
 #include <esp_camera.h>
+#include <U8x8lib.h>
 
 #include "pins.h"
 
@@ -45,7 +46,7 @@ static camera_config_t camera_config = {
     .ledc_channel = LEDC_CHANNEL_0,
 
     .pixel_format = PIXFORMAT_JPEG, // YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QCIF,       // QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+    .frame_size = FRAMESIZE_QVGA,       // QQVGA-UXGA Do not use sizes above QVGA when not JPEG
     .jpeg_quality = 10,
 
     // 0-63 lower number means higher quality
@@ -54,22 +55,28 @@ static camera_config_t camera_config = {
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE);
+
 bool writeImageFile(fs::FS &fs, const char *path, uint8_t *data, size_t len)
 {
+  u8x8.printf("Writing file: %s\n", path);
   Serial.printf("Writing file: %s\n", path);
 
   File file = fs.open(path, FILE_WRITE);
   if (!file)
   {
+    u8x8.println("Failed to open file for writing");
     Serial.println("Failed to open file for writing");
     return false;
   }
   if (file.write(data, len) == len)
   {
+    u8x8.println("File written");
     Serial.println("File written");
   }
   else
   {
+    u8x8.println("Write failed");
     Serial.println("Write failed");
     return false;
   }
@@ -85,6 +92,7 @@ bool ei_image_init(void)
   esp_err_t err = esp_camera_init(&camera_config);
   if (err != ESP_OK)
   {
+    u8x8.printf("Camera init failed with error 0x%x\n", err);
     Serial.printf("Camera init failed with error 0x%x\n", err);
     return false;
   }
@@ -96,10 +104,14 @@ bool ei_image_init(void)
 
 bool ei_image_capture()
 {
+  u8x8.clear();
+  u8x8.setCursor(0, 0);
+
   camera_fb_t *fb = esp_camera_fb_get();
 
   if (!fb)
   {
+    u8x8.println("Camera capture failed\n");
     Serial.println("Camera capture failed\n");
     return false;
   }
@@ -128,18 +140,30 @@ void refreshIndex(void)
 
 void setup()
 {
+  u8x8.setI2CAddress(0x78);
+  u8x8.begin();
+  u8x8.setFlipMode(1);
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+  Wire.begin();
+
   ei_image_init();
 
   pinMode(buttonPin, INPUT_PULLUP);
-
+  
   Serial.begin(9600);
   pinMode(D2, OUTPUT);
+  
+  u8x8.clear();
+  u8x8.setCursor(0, 0);
+  
   if (!SD.begin(D2))
   {
+    u8x8.println("initialization \n failed!");
     Serial.println("initialization \n failed!");
     return;
   }
   refreshIndex();
+  u8x8.println("initialization \n done.");
   Serial.println("initialization \n done.");
   delay(2000);
 }
@@ -150,6 +174,7 @@ void loop()
 
   if(buttonState == LOW){
     ei_image_capture();
+    u8x8.println("OK");
     Serial.println("OK");
   }
 }
